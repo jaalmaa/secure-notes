71dbd7d0857af635d83291176e232cd730ba51a0
jaalma <james@jmaclachlan.me>
2024-07-31 20:36:19 -0400

    remove leftovers from testing

--- src/app/notes/layout.tsx
+++ src/app/notes/layout.tsx
@@ -2,18 +2,13 @@ import { Navbar } from "~/app/components/navbar";
 
 export default function NotesLayout({
   children,
-  modal,
 }: {
   children: React.ReactNode;
-  modal: React.ReactNode;
 }) {
   return (
     <div className="flex flex-col h-full max-h-full w-full max-w-full bg-gray-100">
       <Navbar />
-      <section className="h-full w-full">
-        {modal}
-        {children}
-      </section>
+      <section className="h-full w-full">{children}</section>
     </div>
   );
 }
