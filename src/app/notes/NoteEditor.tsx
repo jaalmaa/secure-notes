cfc8e8413cedb16d9e4ba19f7bd52d84840ae106
jaalma <james@jmaclachlan.me>
2024-07-31 22:01:38 -0400

    fix converting undefined input to defined input error

--- src/app/notes/NoteEditor.tsx
+++ src/app/notes/NoteEditor.tsx
@@ -2,16 +2,32 @@
 import { handleNoteSubmit } from "~/app/notes/NotesActions";
 import { useFormState } from "react-dom";
 import { useState } from "react";
+import { UUID } from "crypto";
+
+interface NoteEditorProps {
+  id: UUID | string;
+  title: string;
+  content: string;
+}
+
+const defaultProps: NoteEditorProps = {
+  id: "",
+  title: "",
+  content: "",
+};
+
+// TODO: Figure out how to type this!
 
 export function NoteEditor(props: { note?: any }) {
   const [errorMessage, noteSubmit] = useFormState(handleNoteSubmit, null);
-  const initialNote = props.note;
+  const initialNote = props ? props.note : defaultProps;
+
   const [note, setNote] = useState(initialNote);
   const handleTitleChange = (event: React.FormEvent<HTMLInputElement>) => {
-    setNote({ title: event.currentTarget.value });
+    setNote({ title: event.currentTarget.value, content: note?.content });
   };
   const handleContentChange = (event: React.FormEvent<HTMLTextAreaElement>) => {
-    setNote({ content: event.currentTarget.value });
+    setNote({ title: note?.title, content: event.currentTarget.value });
   };
 
   return (
@@ -27,16 +43,16 @@ export function NoteEditor(props: { note?: any }) {
         )}
         <input
           name="title"
-          value={note?.title}
+          value={note?.title ? note.title : ""}
           placeholder="enter a title..."
           className="p-2 w-full h-1/8 focus:outline-none text-2xl font-semibold"
           onChange={handleTitleChange}
-        ></input>
+        />
         <textarea
           className="w-full flex-1 shadow-sm p-2 focus:outline-none"
           placeholder="Begin typing your note..."
           name="content"
-          value={note?.content}
+          value={note?.content ? note.content : ""}
           onChange={handleContentChange}
         ></textarea>
         <button
