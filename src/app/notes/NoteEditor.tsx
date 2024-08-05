96427b81f9206c8e8d8db821f9ddd4a3f5e71697
jaalma <james@jmaclachlan.me>
2024-08-05 14:15:16 -0400

    remove delete button from new notes

--- src/app/notes/NoteEditor.tsx
+++ src/app/notes/NoteEditor.tsx
@@ -49,7 +49,7 @@ export function NoteEditor(props: { note: NoteEditorProps }) {
             >
               Save
             </button>
-            <DeleteNoteButton />
+            {initialNote.id ? <DeleteNoteButton /> : ""}
           </div>
         </div>
         <textarea
